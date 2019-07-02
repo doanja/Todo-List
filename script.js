c2f54b34b9430319edabfe501a5291eb712ef422
John Doan <doanja@plu.edu>
2019-07-01 20:05:55 -0700

    Delete button deletes li

--- script.js
+++ script.js
@@ -1,42 +1,44 @@
-var ul = document.getElementById("todoList");
-
-// listen for button click
+// listens for button click
 document.getElementById("addButton").addEventListener("click", addItem);
-
-//document.getElementById("delete").addEventListener("click", deleteItem);
 
 // when button is press, add a <li>
 function addItem(){
-    var text = document.getElementById("input_text").value;         // grabs input text
+    var text = document.getElementById("input_text").value; // grabs input text
     
     var node = document.createElement("li");                // create <li>
     var textnode = document.createTextNode(text);           // creates the text
     node.appendChild(textnode);                             // append text to <li>
-    createDelBtn(node);
-    createEditlBtn(node);
+    createDelBtn(node);                                     // creates and appends <button>
+    //createEditlBtn(node);
 
     // if nothing was inputted...
     if(text === ''){
         alert("Enter something...");
     }
-    else {
+    else { // else add the <li> to the <ul>
         document.getElementById("todoList").appendChild(node);  // append <li> to <ul>
-        document.getElementById("input_text").value = '';               // clear li input field    
+        document.getElementById("input_text").value = '';       // clears input
     }
 }
 
 function createDelBtn(node){
-    var td = document.createElement("button");      // create <button>
-    td.id = "delete";                               // tags the delete button
-    td.innerHTML = "delete";                        // button's text
-    node.appendChild(td);
+    var button = document.createElement("button");      // create <button>
+    button.id = "delete";                               // tags the delete button
+    button.innerHTML = "delete";                        // button's text
+    node.appendChild(button);                           // append this <button> to the node <li>
+    
+    // handles delete functionality of the delete button
+    button.onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
 }
 
 function createEditlBtn(node){
-    var td = document.createElement("button");      // create <button>
-    td.id = "edit";                               // tags the delete button
-    td.innerHTML = "edit";                        // button's text
-    node.appendChild(td);
+    var button = document.createElement("button");    // create <button>
+    button.id = "edit";                               // tags the edit button
+    button.innerHTML = "edit";                        // button's text
+    node.appendChild(button);
 }
 
 function deleteItem(){
