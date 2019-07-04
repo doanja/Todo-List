9d9cd84c3d7d07aef8c090e56e33853d5ee7d175
John Doan <doanja@plu.edu>
2019-07-04 16:34:07 -0700

    Created a span for ok and allowed <p> to be editable

--- script.js
+++ script.js
@@ -7,64 +7,54 @@ function addItem(){
     
     let li = document.createElement("li");                      // creates <li>                                             // 
     createP(li, inputTxt);                                      // creates and puts inputTxt into <p>
-    createDelBtn(li);                                           // creates and appends <span>
+    createDelBtn(li);                                           // creates and appends delete <span>
+    createOKBtn(li);                                            // creates and appends ok <span>
 
     // if nothing was inputted...
     if(inputTxt === ''){
         alert("Enter something...");
     }
     else { // else add the <li> to the <div>
-        let pTag = document.getElementById("pTxt");
-        document.getElementById("list").appendChild(li);    // append <li> to <div>
-        document.getElementById("inputTask").value = '';       // clears input
+        let pTag = document.getElementById("pTxt");             // grabs pTag element
+        document.getElementById("list").appendChild(li);        // append <li> to <div>
+        document.getElementById("inputTask").value = '';        // clears input
     }
 }
 
-// create delete <button> upon creation of a <li>
+// create delete <span> and appends <li>
 function createDelBtn(node){
     let span = document.createElement("span");          // create <span>
     span.id = "delete";                                 // tags the delete span
     span.textContent = "delete";                        // span's text
     node.appendChild(span);                             // append this <span> to the node <li>
     
-
-
     // handles delete functionality of the delete span
     span.onclick = function() {
-        let div = this.parentElement;
-        div.style.display = "none";
+        let div = this.parentElement;                   // <li> is the parent
+        div.style.display = "none";                     // hides <li>
     }
 }
 
-function createP(node, inputTxt){
+// creates a <p> and appends to <li>
+function createP(node, text){
     let p = document.createElement("p");        // creates <p>
     p.id = "pTxt";                              // creates id for <p>
-    p.textContent = inputTxt;                   // puts inputTxt into <p>
+    p.textContent = text;                       // puts inputTxt into <p>
     node.appendChild(p);                        // put <p> into <li>
 
-    let isEditing = false;
-
-    // when <p> is created, create <input>, transfer <p> text into <input>
-    /*
-    on click , create input inside of p and create a ok spann, copy text into input, clear p,
-    on click ok span --> copy input into p, clear and hide input, hide ok span
-    */
     p.onclick = function(){
-        isEditing = true;
-        if(isEditing === true){
-            let inputField = document.createElement("input");
-            inputField.id = "inputField";
-            inputField.textContent = inputTxt;
-            isEditing = false;
-        }
-        else {
-            //isEditing
-        }
+        p.contentEditable = true;               // allow <p> from being editable
+    }
+}
 
-        
+// creates ok <span> and appends to <li>
+function createOKBtn(node){
+    let span = document.createElement("span");  // create <span>
+    span.id = "ok";                             // tags the ok span
+    span.textContent = "ok";                    // span's text
+    node.appendChild(span);                     // append this <span> to node <li>
 
-        // p.innerHTML = node.innerHTML;           // copy <li> text into <p>
-        // node.innerHTML = "";                    // hide <li>
-        // document.getElementById("txt").contentEditable = "true"; // make the <p> editable
+    span.onclick = function() {
+        node.contentEditable = false;           // prevent <p> from being editable
     }
 }
