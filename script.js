dcd5f77b5d939282ac68932bde60e827a39eda18
John Doan <doanja@plu.edu>
2019-07-07 15:47:25 -0700

    Changed how editable list items are handled, used inputs instead of <p> with contentEditable. hides the ok button when not modifying <li>. created an input used for filtering

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+// character limit
 const MAX_INPUT_LENGTH = 40;
 
-// listens for span click
+// listens for add button click
 document.getElementById("addButton").addEventListener("click", addItem);
 
-// when span is press, add a <li>
+/*
+*   renders the <li> item child under <ul>
+*   renders delete <span> used to hide <li>
+*   renders hidden ok <span> used to confirm changes to <p>
+*   clears <input> after clicking ok
+*/ 
 function addItem(){
     let inputTxt = truncateText(document.getElementById("inputTask").value, MAX_INPUT_LENGTH); // grabs input text
     let li = document.createElement("li");                     // creates <li>
@@ -17,16 +23,20 @@ function addItem(){
     else { // else add the <li> to the <div>
         document.getElementById("list").appendChild(li);        // append <li> to <div>
         document.getElementById("inputTask").value = '';        // clears input
-        clearFilter();                                          // show all <li>
+        document.getElementById("ok").style.visibility = "hidden";
     }
 }
 
-// create delete <span> and appends <li>
+/*  
+*   @node, the parent node (<li> in this case)
+*   renders the <span> element and appends it to <li>.
+*   clicking the <span> will hide <li> from the list
+*/
 function createDelBtn(node){
     let span = document.createElement("span");          // create <span>
     span.id = "delete";                                 // tags the delete span
     span.textContent = "delete";                        // span's text
-    node.appendChild(span);                             // append this <span> to the node <li>
+    node.appendChild(span);                             // append it to the parent node
     
     // handles delete functionality of the delete span
     span.onclick = function() {
@@ -35,46 +45,65 @@ function createDelBtn(node){
     }
 }
 
-// creates a <p> and appends to <li>
+/*
+*   @node, the parent node (<li> in this case)
+*   @text, the text to be displayed in <p>
+*   renders the <p> element and appends it to <li>.
+*   clicking <p> will create an <input> field and
+*   remove this element and displays the ok button.
+*/
 function createP(node, text){
     let p = document.createElement("p");        // creates <p>
     p.id = "pTxt";                              // creates id for <p>
-    p.textContent = text;                       // puts inputTxt into <p>
-    node.appendChild(p);                        // put <p> into <li>
-
+    p.textContent = text;                       // sets the text content
+    node.appendChild(p);                        // append it to the parent node
 
     p.onclick = function(){
-        p.contentEditable = true;               // allow <p> from being editable
+        createInput(node,text);                 // creates <input>
+        removeElement("pTxt");                  // removes <p>
+        document.getElementById("ok").style.visibility = "visible"; // display the ok button
     }
-    return p;                                   // returns <p>
 }
 
-// creates ok <span> and appends to <li>
+/*
+*   @node, the parent node (<li> in this case)
+*   @text, the text to be displayed in <input>
+*   renders the <input> and removes <p>
+*/
+function createInput(node, text){
+    let input = document.createElement("input");    // creates <input>
+    input.id = "pInput";                            // creates id for <input>
+    input.textContent = text;                       // sets the text content
+    input.maxLength = MAX_INPUT_LENGTH;             // limits input length
+    node.appendChild(input);                        // append it to the parent node
+}
+
+/* 
+*   @node, the parent node (<li> in this case)
+*   @text, the text to be displayed in <span>
+*   renders the ok <span>
+*   clicking the span will hide this <span>,
+*   the <input>, and renders <p>
+*/
 function createOKBtn(node, text){
     let span = document.createElement("span");  // create <span>
-    span.id = "ok";                             // tags the ok span
-    span.textContent = "ok";                    // span's text
-    node.appendChild(span);                     // append this <span> to node <li>
-    let p = createP(node, text);                // creates and puts inputTxt into <p>
-    
-    p.addEventListener("keydown", disableNewLines);    // disable enter key / new lines
+    span.id = "ok";                             // creats id for <span>
+    span.textContent = "ok";                    // sets the text content
+    node.appendChild(span);                     // append it to the parent node
+    createP(node, text);                        // creates and puts text into <p>
 
     span.onclick = function() {
-        p.contentEditable = false;              // prevent <p> from being editable
-        p.textContent = truncateText(p.textContent, MAX_INPUT_LENGTH);
+        createP(node,document.getElementById("pInput").value);      // renders the <p> with text from <input
+        removeElement("pInput");                                    // removes the <input>
+        document.getElementById("ok").style.visibility = "hidden";  // hides the ok <span>
     }
 }
 
-// prevents new lines from being created when content is editable
-function disableNewLines(e) {
-    if (e.keyCode === 13) {
-        this.contentEditable = false;           // <p> no longer editable
-    }
-}
-
-// function to filter <li> (non-case sensitive)
+/*
+* function to filter <li> (non-case sensitive)
+*/
 function filterList(){
-    let inputTxt = document.getElementById("inputTask").value;          // grabs input text 
+    let inputTxt = document.getElementById("inputFilter").value;        // grabs input text 
     let inputTxtUpperCase = inputTxt.toUpperCase();                     // changes input to upper case
     let list = document.getElementById("list");                         // gets our <div> list
     let li = list.getElementsByTagName('li');                           // grabs the <li>
@@ -93,19 +122,23 @@ function filterList(){
     }
 }
 
-// clears the filter for <li>
-function clearFilter(){
-    let list = document.getElementById("list");     // gets our <div> list
-    let li = list.getElementsByTagName('li');       // grabs the <li>
-    for(i = 0; i < li.length; i++){                 // for all <li>
-        li[i].style.display = "";                   // display the <i>
-    }
-}
-
-// 
+/*
+*   @text, the text to be trimmed
+*   @maxLength, the maximum number of characters
+*   returns the text trimmed down to the maxLength
+*/
 function truncateText(text, maxLength) {
-    if (text.length > maxLength) {
-        text = text.substr(0,maxLength);
+    if (text.length > maxLength) {          // if text length is greater than maxLength
+        text = text.substr(0,maxLength);    // trim the text
     }
     return text;
+}
+
+/*
+*   @elementId, the child element to be rmeoved
+*   removes the child element
+*/
+function removeElement(elementId){
+    let elem = document.getElementById(elementId);
+    elem.parentElement.removeChild(elem);
 }
