98c4d9b22521ec35edffd81f5287ba982133a244
John Doan <doanja@plu.edu>
2019-08-06 13:11:23 -0700

    adding passport auth #2

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const path = require("path");
 const mongoose = require("mongoose");
 const db = require("./config/keys").MongoURI; // db config
 const expressLayouts = require('express-ejs-layouts');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-// connect to database
+// Passport Config
+require('./config/passport')(passport);
+
+// Connect to database
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log('MongoDB connected...'))
@@ -17,17 +24,41 @@ mongoose
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
-// bodyparser middleware
+// Bodyparser middleware
 app.use(express.json()); // handles json data for post requests
 app.use(express.urlencoded({ extended: false })); // handles url encoded data
 
-// sets public as the static folder (?)
-app.use(express.static(path.join('/routes', "public")));
+// Express Session middleware
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
 
-// router to the API
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect-flash (for displaying flash messages)
+app.use(flash());
+
+// Global variables (for our own middleware)
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  // res.locals.error = req.flash('error');
+  next();
+});
+
+// sets public as the static folder (?)
+// app.use(express.static(path.join(__dirname, "public")));
+
+// Routes to the API
 // app.use("/api/routes", require("./routes/api/routes")); // route to the to do list
 app.use('/', require('./routes/api/index'));
 app.use('/users', require('./routes/api/users'));
-app.use('/routes', require('./routes/api/routes'));
+app.use('/dashboard', require('./routes/api/dashboard'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
