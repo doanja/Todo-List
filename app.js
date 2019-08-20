4efede6637d48c72f3acca81b6229b53efbe4672
John Doan <doanja@plu.edu>
2019-08-20 12:31:23 -0700

    created home and login views, create routes for auth and index

--- app.js
+++ app.js
@@ -16,10 +16,19 @@ mongoose
 app.use(express.json()); // handles json data for post requests
 app.use(express.urlencoded({ extended: false })); // handles url encoded data
 
-// sets public as the static folder
-app.use('/', express.static(path.join(__dirname, 'public')));
+// set up view engine
+app.set('view engine', 'ejs');
 
-// router to the API
-app.use("/", require("./routes/list"));
+// home route
+app.use('/', require('./routes/index'));
+
+// auth route
+app.use('/auth', require('./routes/auth'));
+
+// // sets public as the static folder
+// app.use('/', express.static(path.join(__dirname, 'public')));
+
+// // router to the API
+// app.use("/", require("./routes/list"));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
