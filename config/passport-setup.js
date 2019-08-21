d8db4721373904d5daf4dcd718580476de906c68
John Doan <doanja@plu.edu>
2019-08-21 13:32:11 -0700

    added serialization and deserialization to passport-setup.js

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -3,28 +3,52 @@ const GoogleStrat = require('passport-google-oauth20');
 const keys = require('./keys');
 const User = require('../models/user');
 
+passport.serializeUser((user, done) =>{
+    done(null, user.id);
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+     .then((data) => {
+        done(null, user.id);
+     })
+    .catch((err) => {
+        console.log('error deserializing user:', err)
+    })
+})
+
 passport.use(
     new GoogleStrat({
         // options for the Google strat
         callbackURL: '/auth/google/redirect',
         clientID: keys.google.clientID,
         clientSecret: keys.google.clientSecret
-    }, (accessToken,refreshToken, profile, done) => {
-        // passport callback function
-        console.log('passport callback function fired');
-        
+    }, (accessToken, refreshToken, profile, done) => { // passport callback function        
         // need to check DB before creating a new user
+        User.findOne({ googleId: profile.id })
+            .exec()
+            .then(data => {
+                if (data) { // user exists in DB already
+                    console.log('user found in DB:', data)
+                    done(null, data) // serializes the user
+                } else { // creating new user in DB                    
+                    const user = new User({
+                        username: profile._json.name,
+                        googleId: profile._json.sub
+                    }).save()
+                        .then(data => {
+                            console.log('user created success:', data);
+                            done(null, data) // serializes the user
+                        })
+                        .catch(err => {
+                            console.log('error creating user:', err);
+                        });
+                }
+            })
+            .catch(err => {
+                console.log('error occured finding user in the DB:', err);
+            });
 
-        // creating new user in DB
-        const user = new User({
-            username: profile._json.name,
-            googleId: profile._json.sub
-          }).save()
-          .then(data => {
-            console.log('user created success:', data);
-          })
-          .catch(err => {
-            console.log('error creating user:', err);
-          });
+
     })
 )
