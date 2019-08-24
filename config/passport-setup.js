758c64ad4064f9dbf1d32b3a32e360bf07281df3
John Doan <doanja@plu.edu>
2019-08-23 19:48:52 -0700

    list now dynamically grabs the user's todo list

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -4,56 +4,56 @@ const keys = require('./keys');
 const User = require('../models/user');
 
 passport.serializeUser((user, done) => {
-  done(null, user.id);
+	done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-  User.findById(id)
-    .then(data => {
-      done(null, data);
-    })
-    .catch(err => {
-      console.log('error deserializing user:', err);
-    });
+	User.findById(id)
+		.then((data) => {
+			done(null, data);
+		})
+		.catch((err) => {
+			console.log('error deserializing user:', err);
+		});
 });
 
 passport.use(
-  new GoogleStrat(
-    {
-      // options for the Google strat
-      callbackURL: '/auth/google/redirect',
-      clientID: keys.google.clientID,
-      clientSecret: keys.google.clientSecret
-    },
-    (accessToken, refreshToken, profile, done) => {
-      // passport callback function
-      // check DB before creating a new user
-      User.findOne({ googleId: profile.id })
-        .exec()
-        .then(data => {
-          if (data) {
-            // user exists in DB already
-            console.log('user found in DB:', data);
-            done(null, data); // serializes the user
-          } else {
-            // creating new user in DB
-            const user = new User({
-              username: profile._json.name,
-              googleId: profile._json.sub
-            })
-              .save()
-              .then(data => {
-                console.log('user created success:', data);
-                done(null, data); // serializes the user
-              })
-              .catch(err => {
-                console.log('error creating user:', err);
-              });
-          }
-        })
-        .catch(err => {
-          console.log('error occured finding user in the DB:', err);
-        });
-    }
-  )
+	new GoogleStrat(
+		{
+			// options for the Google strat
+			callbackURL: '/auth/google/redirect',
+			clientID: keys.google.clientID,
+			clientSecret: keys.google.clientSecret
+		},
+		(token, tokenSecret, profile, done) => {
+			// passport callback function
+			// check DB before creating a new user
+			User.findOne({ googleId: profile.id })
+				.exec()
+				.then((data) => {
+					if (data) {
+						// user exists in DB already
+						console.log('user found in DB:', data);
+						done(null, data); // serializes the user
+					} else {
+						// creating new user in DB
+						const user = new User({
+							username: profile._json.name,
+							googleId: profile._json.sub
+						})
+							.save()
+							.then((data) => {
+								console.log('user created success:', data);
+								done(null, data); // serializes the user
+							})
+							.catch((err) => {
+								console.log('error creating user:', err);
+							});
+					}
+				})
+				.catch((err) => {
+					console.log('error occured finding user in the DB:', err);
+				});
+		}
+	)
 );
